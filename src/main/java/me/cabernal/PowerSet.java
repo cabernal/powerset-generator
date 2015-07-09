94bafe904a990da2bb7849164dab85b8e170b21b
Camilo Bernal Galindo <camilobern@gmail.com>
2015-07-09 01:15:37 -0400

    Power set generation from input file implemented

--- src/main/java/me/cabernal/PowerSet.java
+++ src/main/java/me/cabernal/PowerSet.java
@@ -1,8 +1,8 @@
 package me.cabernal;
 
 import java.util.BitSet;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class PowerSet<T> implements Iterable<Set<T>>, Iterator<Set<T>> {
@@ -20,7 +20,7 @@ public class PowerSet<T> implements Iterable<Set<T>>, Iterator<Set<T>> {
 	}
 
 	public Set<T> next() {
-		Set<T> subset = new LinkedHashSet<T>();
+		Set<T> subset = new HashSet<T>();
 		// get all subsets in subset combination represented by bit mask
 		for(int i = 0; i < array.length; i++){
 			if(mask.get(i)){
--- src/main/java/me/cabernal/PowerSetApp.java
+++ src/main/java/me/cabernal/PowerSetApp.java
@@ -1,19 +1,31 @@
 package me.cabernal;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * Hello world!
- *
- */
 public class PowerSetApp 
 {
+	public enum Delimeter {
+		CSV(","),
+		TSV("\t"),
+		NEW_LINE("\n");
+		
+		private final String delimeter;
+		private Delimeter(String delimeter) {
+			this.delimeter = delimeter;
+		}
+		public String getDelimeter() {
+			return delimeter;
+		}
+	}
+
     public static void main( String[] args )
     {
     	if(args.length != 2){
@@ -28,8 +40,12 @@ public class PowerSetApp
     	 * - Verify file paths
     	 * - Use string builder
     	 * - Add third argument: set file format
+    	 * - Create output file if it dne
+    	 * - Finally Close
+    	 * - Create FileSet(inFile, outFile, delimeter)
+    	 * - Change class name
     	 */
-    	
+
     	try {
 			BufferedReader reader = Files.newBufferedReader(inputFile);
 			
@@ -37,14 +53,26 @@ public class PowerSetApp
 			String line = null;
 			Set<String> inputSet = new HashSet<String>();
 			while ((line = reader.readLine()) != null){
+				System.out.println("READING: " + line);
 				inputSet.add(line);
 			}
 			
 			PowerSet<String> powerSet = new PowerSet<String>(inputSet);
+			Charset charset = Charset.forName("US-ASCII");
+			BufferedWriter writer = Files.newBufferedWriter(outputFile, charset);
 			
+			for(Set<String> subset : powerSet){
+				//write subset to file
+				String[] subsetArray = new String[subset.size()];
+				subset.toArray(subsetArray);
+				String subsetString = String.join(",", subsetArray);
+				System.out.println("WRITING: " + subsetString);
+				writer.write(subsetString);
+			}
 			
 		} catch (IOException e) {
 			System.err.format("IOException: %s%n", e);
+			//TODO: Finally close
 		}
     	
     			
