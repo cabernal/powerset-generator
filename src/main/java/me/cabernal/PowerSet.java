729418cdfa6c20bbe63eb6d4d197d486cd843a14
Camilo Bernal Galindo <camilobern@gmail.com>
2015-07-09 00:16:59 -0400

    Implemented power set algorithm using iterators

---
+++ src/main/java/me/cabernal/PowerSet.java
@@ -0,0 +1,53 @@
+package me.cabernal;
+
+import java.util.BitSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class PowerSet<T> implements Iterable<Set<T>>, Iterator<Set<T>> {
+	private T[] array = null;
+	private BitSet mask = null;
+	
+	public PowerSet(Set<T> set){
+		this.array = (T[])set.toArray();
+		// extra bit to signal end of bit set
+		this.mask = new BitSet(array.length + 1);
+	}
+
+	public boolean hasNext() {
+		return !mask.get(array.length);
+	}
+
+	public Set<T> next() {
+		Set<T> subset = new LinkedHashSet<T>();
+		// get all subsets in subset combination represented by bit mask
+		for(int i = 0; i < array.length; i++){
+			if(mask.get(i)){
+				subset.add(array[i]);
+			}
+		}
+		
+		incrementMaskValue();
+
+		return subset;
+	}
+
+	public Iterator<Set<T>> iterator() {
+		return this;
+	}
+	
+	private void incrementMaskValue(){
+		for(int i = 0; i < mask.size(); i++){
+			// bit is unset, do not carry 
+			if(!mask.get(i)){
+				mask.set(i);
+				break;
+			}
+			// bit is set, carry the 1
+			mask.clear(i);
+
+		}
+	}
+
+}
--- src/main/java/me/cabernal/PowerSetApp.java
+++ src/main/java/me/cabernal/PowerSetApp.java
@@ -1,5 +1,13 @@
 package me.cabernal;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Hello world!
  *
@@ -10,9 +18,37 @@ public class PowerSetApp
     {
     	if(args.length != 2){
     		System.err.println("usage: <program> <input-file> <output-file>");
+    		System.exit(1);
     	}
-        for (String s: args){
-        	System.out.println(s);
-        }
+    	Path inputFile = Paths.get(args[0]);
+    	Path outputFile = Paths.get(args[1]);
+    	
+    	/*
+    	 * TODO:
+    	 * - Verify file paths
+    	 * - Use string builder
+    	 * - Add third argument: set file format
+    	 */
+    	
+    	try {
+			BufferedReader reader = Files.newBufferedReader(inputFile);
+			
+			// Assume newline separated values as our set format
+			String line = null;
+			Set<String> inputSet = new HashSet<String>();
+			while ((line = reader.readLine()) != null){
+				inputSet.add(line);
+			}
+			
+			PowerSet<String> powerSet = new PowerSet<String>(inputSet);
+			
+			
+		} catch (IOException e) {
+			System.err.format("IOException: %s%n", e);
+		}
+    	
+    			
+    	
+    	
     }
 }
